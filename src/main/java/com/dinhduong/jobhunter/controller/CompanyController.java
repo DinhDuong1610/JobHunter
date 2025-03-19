f3a3e64c2e70afeb8abf9b25e2652071f60ea591
Dinh Duong <jenny.180820@gmail.com>
2025-03-19 23:31:50 +0700

    add version api

--- src/main/java/com/dinhduong/jobhunter/controller/CompanyController.java
+++ src/main/java/com/dinhduong/jobhunter/controller/CompanyController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,7 @@ import com.dinhduong.jobhunter.service.CompanyService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api/v1")
 public class CompanyController {
     private final CompanyService companyService;
 
