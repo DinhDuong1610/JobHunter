1f06eb17982855717d2e1604378b05756c2bfc70
Dinh Duong <jenny.180820@gmail.com>
2025-05-02 03:25:34 +0700

    update delete-company

--- src/main/java/com/dinhduong/jobhunter/repository/UserRepository.java
+++ src/main/java/com/dinhduong/jobhunter/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.dinhduong.jobhunter.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.dinhduong.jobhunter.domain.Company;
 import com.dinhduong.jobhunter.domain.User;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     User findByRefreshTokenAndEmail(String token, String email);
+
+    List<User> findByCompany(Company company);
 }
